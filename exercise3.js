12cd80b59334822455e14847197d06006bc14213
noeysoftware <noeysoftware@gmail.com>
2024-05-15 16:11:06 +0700

    Create exercise3.js

---
+++ exercise3.js
@@ -0,0 +1,11 @@
+//Write a JavaScript program to construct the following pattern, using a nested for loop.
+for (let i = 0; i < 5 ; i++){
+	let k = ""
+
+	for (let j = 0; j <= i; j++)
+	{
+	k=k+"*"
+	}
+    
+    console.log(k);
+};
